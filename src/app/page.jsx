0172cf616aeadcbe2bb90eef6285d1f1283364a3
Talha0020 <talha.ahmed0020@gmail.com>
2023-08-13 23:23:13 +0500

    abc

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,8 +8,8 @@ export default function Home() {
         <div className='flex flex-col sm:flex-row gap-10 justify-center mt-10'>
           <Link href='https://weather-app-two-olive.vercel.app/' target='_block'>
           <Image
-          className="transition-transform transform hover:scale-110"
-          alt=''
+          className="transition-transform transform hover:scale-110 rounded-md"
+          alt='Loading Error'
           src='/1.png'
           width={400}
           height={400}
@@ -18,8 +18,8 @@ export default function Home() {
 
           <Link href='https://gpt3-theta-one.vercel.app/' target='_block'>
           <Image
-          className="transition-transform transform hover:scale-110"
-          alt=''
+          className="transition-transform transform hover:scale-110 rounded-md"
+          alt='Loading Error'
           src='/2.png'
           width={400}
           height={400}
@@ -28,8 +28,8 @@ export default function Home() {
 
           <Link href='https://www.hextechdevelopers.com/' target='_block'>
           <Image
-          className="transition-transform transform hover:scale-110"
-          alt=''
+          className="transition-transform transform hover:scale-110 rounded-md"
+          alt='Loading Error'
           src='/3.png'
           width={400}
           height={400}
@@ -41,8 +41,8 @@ export default function Home() {
         <div className='flex flex-col sm:flex-row gap-10 justify-center mt-10'>
           <Link href='https://cylock.vercel.app/' target='_block'>
           <Image
-          className="transition-transform transform hover:scale-110"
-          alt=''
+          className="transition-transform transform hover:scale-110 rounded-md"
+          alt='Loading Error'
           src='/4.png'
           width={400}
           height={400}
@@ -51,8 +51,8 @@ export default function Home() {
 
           <Link href='https://acm-6i1p.vercel.app/' target='_block'>
           <Image
-          className="transition-transform transform hover:scale-110"
-          alt=''
+          className="transition-transform transform hover:scale-110 rounded-md"
+          alt='Loading Error'
           src='/5.png'
           width={400}
           height={400}
@@ -61,8 +61,8 @@ export default function Home() {
 
           <Link href='https://tiny-squirrel-88bdc9.netlify.app/' target='_block'>
           <Image
-          className="transition-transform transform hover:scale-110"
-          alt=''
+          className="transition-transform transform hover:scale-110 rounded-md"
+          alt='Loading Error'
           src='/6.png'
           width={400}
           height={400}
@@ -74,8 +74,8 @@ export default function Home() {
         <div className='flex flex-col sm:flex-row gap-10 justify-center mt-10'>
           <Link href='https://cozy-shortbread-d79368.netlify.app/' target='_block'>
           <Image
-          className="transition-transform transform hover:scale-110"
-          alt=''
+          className="transition-transform transform hover:scale-110 rounded-md"
+          alt='Loading Error'
           src='/7.png'
           width={400}
           height={400}
